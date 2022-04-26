6e01651d06e61b1f1e7629c8160c9973a2e39f02
Top Coder <61309193+topcoder1208@users.noreply.github.com>
2022-04-26 04:30:41 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { App } from "./app";
 
 async function main() {
-    const port = process.env.APP_LISTEN_PORT || 5000;
+    const port = process.env.APP_LISTEN_PORT || 5001;
     const app = new App(port);
     await app.listen();
 }
